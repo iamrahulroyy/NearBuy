00a75dd6f5dfff0c88254f57890eccf3f5a7d830
Rahul Roy <rahulroy.ac.in@gmail.com>
2025-11-25 16:51:41 +0530

    Added export const dynamic = 'force-dynamic' to src/app/shops/page.tsx  to force dynamic rendering.

--- frontend/src/app/shops/page.tsx
+++ frontend/src/app/shops/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
+
 import { useEffect, useState, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import axios from 'axios';
